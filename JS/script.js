ed917b552107e248fcac1f4019b9dd893b707b72
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-07-31 10:02:19 +0600

    added AOS animation

--- JS/script.js
+++ JS/script.js
@@ -12,6 +12,16 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+// typing text
+
+const typed = new Typed('.multiple-text', {
+      strings: ['Bodybuilding', 'PowerLifting', 'Strength Training', 'Physical Fitness'],
+      typeSpeed: 60,
+      backSpeed: 60,
+      backDelay: 1000,
+      loop: true,
+    });
+
 
 // modal section
   const joinBtn = document.querySelector('.navBtn');
