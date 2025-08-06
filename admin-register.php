f7fd4f2112f2e2fc5868f919a6fe0e0f37aab69b
Diganta <amiadi123@outlook.com>
2025-08-06 22:34:20 +0600

    php setup

---
+++ admin-register.php
@@ -0,0 +1,19 @@
+<?php
+include 'connect.php';
+
+$username = mysqli_real_escape_string($con, $_POST['adminRegisterUsername']);
+$email = mysqli_real_escape_string($con, $_POST['AdminRegisterEmail']);
+$role = mysqli_real_escape_string($con, $_POST['adminRole']);
+$password = mysqli_real_escape_string($con, $_POST['adminRegisterPassword']);
+
+$query = "INSERT INTO admins (username, email, role, password)
+          VALUES ('$username', '$email', '$role', '$password')";
+
+$run = mysqli_query($con, $query);
+
+if (!$run) {
+    echo "Admin registration failed: " . mysqli_error($con);
+} else {
+    header("location: admin-login.html?status=success");
+}
+?>
